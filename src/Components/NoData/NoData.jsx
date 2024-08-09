9506120b5198772c1239fa808afecfa0691db7e1
Amina AbdelAzeez <aminaabdelazeez99@gmail.com>
2024-08-09 11:23:55 +0300

    my changes

---
+++ src/Components/NoData/NoData.jsx
@@ -0,0 +1,8 @@
+import { Empty } from "antd";
+import React from "react";
+
+function NoData() {
+  return <Empty style={{ width: "200px", margin: "100px auto" }} />;
+}
+
+export default NoData;
--- src/Components/ProductsList/ProductsList.jsx
+++ src/Components/ProductsList/ProductsList.jsx
@@ -3,15 +3,24 @@ import { useState } from "react";
 import { Card, Flex, Image, Tag } from "antd";
 import { Link } from "react-router-dom";
 import { DoubleRightOutlined } from "@ant-design/icons";
+import NoData from "../NoData/NoData";
 const { Meta } = Card;
 
 function ProductsList() {
   const [products, setProducts] = useState([]);
+  const [noData, setNoData] = useState(false);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((json) => setProducts(json));
+  }, []);
+
+  useEffect(() => {
+    setNoData(true);
+    setTimeout(() => {
+      setNoData(false);
+    }, 1500);
   }, []);
 
   return (
@@ -22,45 +31,53 @@ function ProductsList() {
       wrap
       style={{ marginBottom: "30px" }}
     >
-      {products.map((product) => {
-        return (
-          <Card
-            key={product.id}
-            hoverable
-            style={{
-              width: 240,
-              height: 480,
-              marginBottom: 15,
-              padding: 15,
-            }}
-            cover={
-              <Image
-                alt={product.description}
-                src={product.image}
+      {noData ? (
+        <NoData />
+      ) : (
+        products.map((product) => {
+          return (
+            <Card
+              key={product.id}
+              hoverable
+              style={{
+                width: 240,
+                height: 480,
+                marginBottom: 15,
+                padding: 15,
+              }}
+              cover={
+                <Image
+                  alt={product.description}
+                  src={product.image}
+                  style={{
+                    height: 200,
+                  }}
+                />
+              }
+            >
+              <Meta
+                title={product.title.slice(0, 17)}
+                style={{ marginBottom: 10 }}
+              />
+              <Meta
+                description={product.description.slice(0, 160)}
                 style={{
-                  height: 200,
+                  textAlign: "left",
+                  fontSize: 13,
+                  marginBottom: "10px",
                 }}
               />
-            }
-          >
-            <Meta
-              title={product.title.slice(0, 17)}
-              style={{ marginBottom: 10 }}
-            />
-            <Meta
-              description={product.description.slice(0, 160)}
-              style={{ textAlign: "left", fontSize: 13, marginBottom: "10px" }}
-            />
-            <Tag color="#cd201f" style={{ margin: "0px 10px 10px 0px" }}>
-              ${product.price}
-            </Tag>
-            <Link color="#3b5999" to={`${product.id}`}>
-              Read More!
-              <DoubleRightOutlined style={{ marginLeft: "4px" }} />
-            </Link>
-          </Card>
-        );
-      })}
+              <Tag color="#cd201f" style={{ margin: "0px 10px 10px 0px" }}>
+                ${product.price}
+              </Tag>
+              <Link color="#3b5999" to={`${product.id}`}>
+                Read More!
+                <DoubleRightOutlined style={{ marginLeft: "4px" }} />
+              </Link>
+            </Card>
+          );
+        })
+      )}
     </Flex>
   );
 }
